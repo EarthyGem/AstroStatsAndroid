6ca98b575eb7e47a97c752b2db7c6bf8f3c90d08
Errick Miron <erricklynne@gmail.com>
2025-09-13 21:02:29 -0700

    no message

--- app/src/main/java/app/lilaverse/astrostatsandroid/APIKeys.kt
+++ app/src/main/java/app/lilaverse/astrostatsandroid/APIKeys.kt
@@ -1,21 +1,7 @@
 package app.lilaverse.astrostatsandroid.chat
 
 object APIKeys {
-    const val OPENAI = "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"
-    const val OPENAI2 = "sk-proj-L1F cIWt4YetrhDSpLHRHXx9B63UgRMcQgssWAanAoQZVU095aoF5PyBsEw65yZLvXgqeVP7xzCT3BlbkFJpZIyGQ1jyZq_sY5t_w-wlr16wSi7caMQQPDjeAGdWF1SrZMhNFO1JscNAENSsZiCngQ-FrNqAA"
-    const val ANTHROPIC = "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"
-    const val HUGGING_FACE = "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"
-    const val GEMINI = "AIzaSyCE51ry4_dHxwfscwC94sLv33Zb_WtIPsk"
-    const val GROK = "xai-U4u0x5gL6JHqqMO6AzXFhj9GQCWv3G7dseuwBKJ2nrfZnUNXsvgsSw8GabnVCpcnGrSl14aFCiQ1RpZ7"
-    const val RUNWAY = "key_dba802e1470978a097a2a7241497cac5c16145bda0937aac865245fe30d49d67e59effdfc1149fc90b0ae6dffacbb1af09f544367db3a7ccd917849c89b9caf9"
-    const val PERPLEXITY = "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"
-    const val ANTHROPIC2 = "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"
-    const val CALENDLY = "eyJraWQiOiIxY2UxZTEzNjE3ZGNmNzY2YjNjZWJjY2Y4ZGM1YmFmYThhNjVlNjg0MDIzZjdjMzJiZTgzNDliMjM4MDEzNWI0IiwidHlwIjoiUEFUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNzQyNzk1MTY5LCJqdGkiOiI0MzE2YjdhOC1hMjBkLTRiZGItOWYwOC02MWYyYWFlODM0YjUiLCJ1c2VyX3V1aWQiOiJBRkhDRlAzRkoyT1BONUxUIn0.0fBneCRenwEu7t0HykgdCBUMBOfR9rIoLn-s-9dPVxwHL_Fk6dO3FHgtYBD0__HdGW_t8n8epCKpVQOR8j0UzQ"
-    const val OPEN_SUBTITLES_USER_AGENT = "VZ0xZoVnFAGTbpf7909Sm3oMzPewD9he"
-    const val TMDB = "b1aedbfebf256f64111704b4d62f1347"
-
-    // Mailchimp credentials
-    const val MAILCHIMP_API_KEY = "524488ea9b3b19a3ff0931918fc5d7c9"
-    const val MAILCHIMP_AUDIENCE_ID = "8ac4f5dad4"
-    const val MAILCHIMP_SERVER_PREFIX = "us19"
+    const val ANTHROPIC_KEY = "your_anthropic_key"
+    const val OPENAI_KEY = "your_openai_key"
+    const val GEMINI_KEY = "your_gemini_key"
 }
